67986267c1fba3932030f6dad3dd40f787d8c350
elaniobro <eitrybuch@gmail.com>
2020-10-30 17:52:07 -0400

    GIT-0: Add css and lint

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,5 +2,8 @@ module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
-  rules: {},
+  rules: {
+    'color-no-invalid-hex': true,
+    'color-hex-length': 'long',
+  },
 }
