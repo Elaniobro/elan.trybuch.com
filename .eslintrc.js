ad0d5a26287248c99b0a9e8f03918fc8c12f747e
elaniobro <eitrybuch@gmail.com>
2020-12-04 21:22:42 -0500

    GIT-0: Update eslint/prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,10 +9,10 @@ module.exports = {
   },
   extends: [
     '@nuxtjs',
-    'prettier',
     'prettier/vue',
     'plugin:prettier/recommended',
-    'plugin:nuxt/recommended'
+    'plugin:nuxt/recommended',
+    'prettier'
   ],
   plugins: ['prettier'],
   // add your custom rules here
